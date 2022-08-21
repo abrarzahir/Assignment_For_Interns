f242907e9340248f2ba2dcd5f9e3d3f274fb87ce
abrarzahir <maxallinterns@gmail.com>
2022-08-21 11:45:31 +0600

    last commit

--- assignment_05.js
+++ assignment_05.js
@@ -1,13 +1,16 @@
-function myVowel(Eunoia){
-    let value = 0;
 
-    for(let i=0;i<Eunoia.length;i++){
-            if(Eunoia[i].toLowerCase() === 'a' || Eunoia[i].toLowerCase() === 'e' || Eunoia[i].toLowerCase() === 'i' || Eunoia[i].toLowerCase() === 'o' || Eunoia[i].toLowerCase() === 'u' ){
-                value += 1;
-            }
-    }
-return value;
-}
+
+
+// function myVowel(Eunoia){
+//     let value = 0;
+
+//     for(let i=0;i<Eunoia.length;i++){
+//             if(Eunoia[i].toLowerCase() === 'a' || Eunoia[i].toLowerCase() === 'e' || Eunoia[i].toLowerCase() === 'i' || Eunoia[i].toLowerCase() === 'o' || Eunoia[i].toLowerCase() === 'u' ){
+//                 value += 1;
+//             }
+//     }
+// return value;
+// }
 
 function myVowelShort(Eunoia){
     let value = 0;
@@ -16,6 +19,8 @@ function myVowelShort(Eunoia){
     for(let i=0;i<Eunoia.length;i++){
             if(vowelArray.indexOf(Eunoia[i]) != -1){
                 value += 1;
+
+                
             }
     }
     return value;
--- assignment_09.js
+++ assignment_09.js
@@ -1,18 +1,18 @@
 const Student = {
   id: 102,
-  firstName: 'labib',
+  firstName: "Labib",
   lastName: "Hasan",
-  phone: 1521334597,
+  phone: "01521334597",
 
   classes: [
     {
-      classId: 102,
+      classId: 100,
       subjectName: "English",
-      obtainMark: 80,
+      obtainMark: 10,
     },
 
     {
-      classId: 103,
+      classId: 12,
       subjectName: "Math",
       obtainMark: 90,
     },
@@ -23,95 +23,135 @@ const Student = {
     line2: "World",
     thana: "Kafrul",
     district: "dhaka",
-    postcode: 8602,
+    postcode: "0123",
   },
 
-  subjectId: [10, 20, 30],
-  dateOfBirth: "15.06.2000",
+  subjectId: [10,20,30],
+  dateOfBirth: "15.06.1998",
 };
 
 function input() {
+  // console.log(Student.phone);
+
   
+  let num1 = Student.phone;
+  let text = num1.toString();
+  let num2 = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  let value = 0;
 
   if (Student.id == undefined) {
     throw "id is required";
   }
-  if (parseInt(Student.id) != Student.id) {
+  if (parseInt(Student.id) !== Student.id) {
     throw "Student id has to be an integer";
   }
 
   if (Student.firstName == undefined) {
     throw "firstName is required";
-  } if (typeof Student.firstName != "string") {
+  }
+  if (typeof Student.firstName != "string") {
     throw "firstName has to be a string";
-  } 
+  }
 
   if (Student.lastName == undefined) {
     throw "lastName is required";
-  }  if (typeof Student.lastName != "string") {
+  }
+  if (typeof Student.lastName != "string") {
     throw "lastName has to be string";
-  } 
+  }
 
   if (Student.phone == undefined) {
     throw "phone number is required";
-  } 
-  
+  }
 
-  if(parseInt(Student.phone) != Student.phone){
-    throw "phone number has to be number";
+  if (Number(Student.phone) != Student.phone) {
+    throw "phone number has to be numeric";
+  }
+
+  // if(text[0] != 0){
+  //   throw "Can be start with zero";
+  // }
+
+  for (let i = 0; i < text.length; i++) {
+    if (num2.indexOf(text[i]) == -1) {
+      value += 1;
+
+      throw "phone number has to be numeric";
+    }
   }
 
   for (const element in Student.classes) {
-    
-
     if (Student.classes[element].classId == undefined) {
       throw "class id is required";
-    } if (parseInt(Student.classes[element].classId) != Student.classes[element].classId) {
+    }
+    if (
+      parseInt(Student.classes[element].classId) !==
+      Student.classes[element].classId
+    ) {
       throw "class id has to be an integer";
-    } 
+    }
     if (Student.classes[element].subjectName == undefined) {
       throw "subject name is required";
-    } if (typeof Student.classes[element].subjectName != "string") {
+    }
+    if (typeof Student.classes[element].subjectName != "string") {
       throw "subject name has to be a string";
-    } 
+    }
 
-    if (Student.classes[element].obtainMark == undefined) {
-      throw "obtain mark is required";
-    } else if (parseInt(Student.classes[element].obtainMark) != Student.classes[element].obtainMark) {
+    if (
+      parseInt(Student.classes[element].obtainMark) !==
+      Student.classes[element].obtainMark
+    ) {
       throw "obtain mark has to be a number";
-    } 
+    }
+    // console.log(Student.classes[element].obtainMark);
+    if (
+      Student.classes[element].obtainMark < 0 ||
+      Student.classes[element].obtainMark > 100
+    ) {
+      throw "obtain mark should between 0 to 100 numbers";
+    }
   }
-
-
 
   if (Student.address.line1 == undefined) {
     throw "line1 is required";
-  } if (typeof Student.address.line1 != "string") {
+  }
+  if (typeof Student.address.line1 != "string") {
     throw "line1 has to be string";
-  } 
-  if (Student.address.line2 == undefined) {
-    throw "line2 is required";
-  } if (typeof Student.address.line2 != "string") {
+  }
+  // if (Student.address.line2 == undefined) {
+  //   throw "line2 is required";}
+  if (typeof Student.address.line2 != "string") {
     throw "line2 has to be string";
-  } 
+  }
 
   if (Student.address.thana == undefined) {
     throw "thana name is required";
-  }  if (typeof Student.address.thana != "string") {
+  }
+  if (typeof Student.address.thana != "string") {
     throw "thana name has to be string";
-  } 
+  }
 
-  if(Student.address.district == undefined){
+  if (Student.address.district == undefined) {
     throw "ditrict name is required";
-  }if (typeof Student.address.district != "string") {
+  }
+  if (typeof Student.address.district != "string") {
     throw "district name has to be string";
-  } 
+  }
 
   if (Student.address.postcode == undefined) {
     throw "post code is required";
-  } if (parseInt(Student.address.postcode) != Student.address.postcode) {
-    throw "postcode has to be number";
-  } 
+  }
+  if (Number(Student.address.postcode) != Student.address.postcode) {
+    throw "postcode has to be numeric";
+  }
+
+  for (let i = 0; i < text.length; i++) {
+    if (num2.indexOf(text[i]) == -1) {
+      value += 1;
+
+      throw "postcode has to be numeric";
+    }
+  }
 
   if (Student.subjectId == undefined) {
     throw "Suject Id is required";
@@ -121,22 +161,19 @@ function input() {
   }
 
   for (let i = 0; i < Student.subjectId.length; i++) {
-    if (Student.subjectId[i] != parseInt(Student.subjectId[i])) {
+    if (Student.subjectId[i] !== parseInt(Student.subjectId[i])) {
       throw "Subject Id has to be an array of integers";
     }
   }
 
-  
-
   if (Student.dateOfBirth == undefined) {
     throw "date of birth is required";
-  }  if (typeof Student.dateOfBirth != "string") {
+  }
+  if (typeof Student.dateOfBirth != "string") {
     throw "date of birth has to be string";
-  } 
+  }
 
   console.log("ALL INPUT IS OK");
 }
-
-
 
 input();
